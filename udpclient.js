1fdab4250ae27d8867a50b78e4cd18b8c115a6a4
Håkon Dahle <hakon.dahle@gmail.com>
2021-03-31 11:32:15 +0200

    added some argv handling

--- udpclient.js
+++ udpclient.js
@@ -1,19 +1,31 @@
-
-
-
 var argv = require('minimist')(process.argv.slice(2));
 
-let port = argv.port || 8080;   // end date
-let ip = argv.ip || 'localhost';
-let msg = argv.msg || 'message';
+const port = argv.port || 8080;
+const ip = argv.ip || 'localhost';
+const msg = argv.msg || 'hello';
 
+if (port === true || ip === true || msg === true) {
+  console.log('Usage: node eia.js [--port <port>] [--ip <ipaddr>] --msg "Message"');
+  console.log('   --port: Default value is 8080')
+  console.log('   --ip: Default value is localhost')
+  console.log('   --msg: Default value is "hello" ')
+  process.exit();
+}
 
-//console.log('Usage: node eia.js [--port <port>] [--ip <ipaddr>]');
-//console.log('   --port: Default value is 8080')
-//console.log('   --ip: Default value is localhost')
-//  process.exit();
-
+console.log('port', port)
+console.log('ip', ip)
+console.log('msg', msg)
 
 var dgram = require('dgram');
-var s = dgram.createSocket('udp4');
-s.send(Buffer.from(msg), port, ip);
+var client = dgram.createSocket('udp4');
+client.send(Buffer.from(msg), port, ip, () => {
+  console.log('udpclient: message sent');
+  setTimeout(() => {
+    client.close();
+  }, 1000)
+});
+
+client.on('error', (err) => {
+  console.log('udpclient error');
+  client.close();
+});
